8dcdcffcc0340e8f52ef2afa390b698376183ddd
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2023-03-27 21:05:29 +0200

    Remove typed_props since they are moved to ya-provider

--- agreement-utils/src/lib.rs
+++ agreement-utils/src/lib.rs
@@ -2,8 +2,6 @@ pub mod agreement;
 mod constraints;
 mod proposal;
 mod template;
-mod typed_props;
 
 pub use agreement::{AgreementView, DemandView, Error, OfferTemplate, OfferView, ProposalView};
 pub use constraints::*;
-pub use typed_props::*;
